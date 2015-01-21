8874490dd08af417f4ae0aaf42b8040f3f0423db
Nikhil Chandra <niqhil.khorwal@gmail.com>
2015-01-20 20:20:53 -0800

    Union-Find Added

--- unionfind/UnionFind.java
+++ unionfind/UnionFind.java
@@ -2,4 +2,41 @@ package unionfind;
 
 public class UnionFind {
 
+	int[] id;
+	int[] sz;
+	int count;
+	public UnionFind(int N){
+		this.count=N;
+		id = new int[N];
+		sz = new int[N];
+		for( int i=0;i<N;i++){
+			id[i]=i;
+			sz[i]=1;
+		}
+	}
+	public int find(int p){
+		while(id[p]!=p)p=id[p];
+		return p;
+	}
+	
+	public boolean connected(int p, int q){
+		return find(p)==find(q);
+	}
+	
+	public void union(int p, int q){
+		int pId = find(p);
+		int qId = find(q);
+		if(pId==qId) return;
+		
+		if(sz[pId]>sz[qId]){
+			id[qId]=pId;
+			sz[pId]+=qId;
+		}
+		
+		if(sz[pId]<sz[qId]){
+			id[pId]=qId;
+			sz[qId]+=pId;
+		}
+		count--;
+	}
 }
